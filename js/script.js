8a444797172a3de6a3b34b41b26ca30ccb13b020
ChaYounglim <myday801@naver.com>
2024-04-03 04:41:56 +0900

    contact section fin

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".pfSwiper", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
